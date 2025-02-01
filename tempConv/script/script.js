370cbbcec0054440fc6cf19e7ffb463a647f2c0f
David <89405283+whiteRabbit1987@users.noreply.github.com>
2025-02-01 11:29:33 -0600

    Temp converter with °

--- tempConv/script/script.js
+++ tempConv/script/script.js
@@ -10,12 +10,12 @@ function convert(){
     if(toFaren.checked){
         temp = Number(inputBox.value);
         temp = temp * 9 / 5 + 32;
-        converted.textContent = temp.toFixed(1) + 'F';
+        converted.textContent = temp.toFixed(1) + '°F';
     }
     else if(toCel.checked){
         temp = Number(inputBox.value);
         temp = (temp - 32) * (5/9);
-        converted.textContent = temp.toFixed(1) + 'C';
+        converted.textContent = temp.toFixed(1) + '°C';
     }
     else {
         converted.textContent = "Select a temperature unit!"
