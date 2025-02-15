42babe112cfd8e45b93f572df688042b72815c1b
David <89405283+whiteRabbit1987@users.noreply.github.com>
2025-02-15 08:46:13 -0600

    Dom event practise

---
+++ domEvents/script/script.js
@@ -0,0 +1,38 @@
+const eventBox = document.getElementById('eventBox');
+
+const move = 10;
+let x = 0;
+let y = 0;
+
+document.addEventListener('keydown', event => {
+    switch (event.key) {
+        case 'ArrowUp':
+            eventBox.style.top = `${y -= move}px`;
+            break;
+        case 'ArrowDown':
+            eventBox.style.top = `${y += move}px`;
+            break;
+        case 'ArrowLeft':
+            eventBox.style.left = `${x -= move}px`;
+            break;
+        case 'ArrowRight':
+            eventBox.style.left = `${x += move}px`;
+            break;
+    }
+});
+
+eventBox.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = 'yellow';
+    event.target.textContent = "Don't! 😥";
+});
+
+eventBox.addEventListener('mouseout', event => {
+    event.target.style.backgroundColor = 'blue';
+    event.target.textContent = "Click me! 😁";
+});
+
+eventBox.addEventListener('click', event => {
+    event.target.style.backgroundColor = 'red';
+    event.target.textContent = 'OUCH! 😵‍💫';
+});
+
--- test/script/script.js
+++
@@ -1,8 +0,0 @@
-const fruits = [{name: 'apple', color: 'red', calories: 95}, 
-                {name: 'banana', color: 'yellow', calories: 105}, 
-                {name: 'orange', color: 'orange', calories: 45}, 
-                {name: 'pineapple', color: 'yellow', calories: 37}];
-
-const yellowFruits = fruits.filter(fruit => fruit.color === 'yellow');
-
-console.log(yellowFruits);
